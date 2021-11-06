04eb8e213aeb707a626057f265570a419cf58273
Yesus0212 <jesussolispadron@gmail.com>
2021-11-05 18:12:51 -0600

    Inicio de estructura en duro, del contenido del post

--- js/scriptContent.js
+++ js/scriptContent.js
@@ -2,3 +2,15 @@
 const urlParams = new URLSearchParams(window.location.search);
 const id = urlParams.get('p')
 
+// Sección de SCORES DINAMICOS
+$('.numbers#reactions').text(5);
+$('.numbers#unicorns').text(10);
+$('.numbers#comments').text(15);
+
+// Dato de imageCover DINAMICO
+const imageCover = "https://res.cloudinary.com/practicaldev/image/fetch/s--DFDVlmMj--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/ztc3fq2x1adkhx39r6ua.png";
+$('img#imgCover').attr({src:imageCover});
+
+$('h1#title').text("TITULO");
+
+// SIGUEN LOS TAGS
